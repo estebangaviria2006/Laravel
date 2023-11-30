6362aa1e72745e599415967f2df1c98a2e9f5d96
Luis1080931 <luisu0616@gmail.com>
2023-11-30 13:56:51 -0500

    segundo cambio

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>@yield('title')</title>
+</head>
+<body>
+
+    <header class="p-5 border-b bg-white shadow flex justify-between items-center">
+        <h1 class="text-3xl font-black">
+            Electrodomesticos full
+        </h1>
+        <nav class="flex gap-2 items-center">
+            <a class="font-bold uppercase text-gray-600" href="{{ route('register') }}"> Crear cuenta </a>
+        </nav>
+    </header>
+    <main class="flex justify-center container mx-auto mt-10">
+        <h2 class="font-black text-center text-3xl mb-10">
+            @yield('titulo')
+        </h2>
+        @yield('contenido')
+    </main>
+</body>
+</html>
